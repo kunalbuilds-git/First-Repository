fd407c612add943f4f8895cd0c110bd1cb0bb91e
Kunal Raghuvanshi <kunalbuildsofficial@gmail.com>
2026-07-04 02:44:07 +0530

    Feat: added polymorphic arrayList in account file

--- Bank/Account.java
+++ Bank/Account.java
@@ -1,12 +1,12 @@
 package Bank;
+import java.util.ArrayList; // Step 1: Imported the utility framework
 
-public class Account implements Transaction{
+public class Account implements Transaction {
     // 1. Core fields set to protected so child classes can use them directly
     protected String accountHolderName;
     protected int accountNumber;
     protected double balance;
     
-
     // 2. Main constructor - sets up name, number, and checks for negative starting balance
     public Account(String name, int accountNo, double accountbalance) {
         this.accountHolderName = name;
@@ -25,7 +25,7 @@ public class Account implements Transaction{
             this.balance = this.balance + amount;
             System.out.println("Amount deposited successfully: $" + amount);
         } else {
-           throw new IllegalArgumentException ("Deposit amount must be greater than zero");
+           throw new IllegalArgumentException("Deposit amount must be greater than zero");
         }        
     }
 
@@ -37,7 +37,7 @@ public class Account implements Transaction{
             this.balance = this.balance - amount;
             System.out.println("Amount withdrew Successfully: $" + amount);
         } else {
-            throw new IllegalArgumentException("Insufficient amountfor this withdrawal!");
+            throw new IllegalArgumentException("Insufficient amount for this withdrawal!");
         }           
     }
 
@@ -57,5 +57,34 @@ public class Account implements Transaction{
 
     public double getBalance() {
         return this.balance;
+    }
+
+    //DAY 70: DYNAMIC ARRAYLIST IMPLEMENTATION
+    public static void main(String[] args) {
+        System.out.println("--- BANKING SYSTEM POLYMORPHIC ARRAYLIST --- \n");
+
+        // 1. Declare a resizable ArrayList that manages references of type 'Account'
+        ArrayList<Account> accounts = new ArrayList<>();
+
+        // 2. Dynamically add your subclass objects to the list
+        accounts.add(new SavingsAccount("Kunal - Savings", 1234, 1000.0, 5.0));
+        accounts.add(new CurrentAccount("Kunal - Current", 5678, 5000.0, 1000.0)); 
+
+        // 3. Loop through dynamic list
+        for (Account acc : accounts) {
+            System.out.println("----------------------------------------");
+            
+            // Triggers overridden method behaviors dynamically
+            acc.displayAccountDetails(); 
+            
+            try {
+                acc.withdraw(400);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+            
+            acc.displayAccountDetails();
+        }
+        System.out.println("----------------------------------------");
     }
 }
